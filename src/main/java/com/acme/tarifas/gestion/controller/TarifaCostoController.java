034694f4ed12d0cd3cbc44cf4554de34f190af2a
francomarinozzi <francomarinozzi4@gmail.com>
2025-06-11 21:58:07 -0300

    cors fix

--- src/main/java/com/acme/tarifas/gestion/controller/TarifaCostoController.java
+++ src/main/java/com/acme/tarifas/gestion/controller/TarifaCostoController.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
+import java.util.List;
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/tarifas")
 public class TarifaCostoController {
--- src/main/java/com/acme/tarifas/gestion/controller/TransportistaController.java
+++ src/main/java/com/acme/tarifas/gestion/controller/TransportistaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/transportistas")
 public class TransportistaController {
