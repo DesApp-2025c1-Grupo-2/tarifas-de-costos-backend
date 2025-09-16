491c0e84a6d789b4ac502bf03b3595a51fd7ee3c
francomarinozzi <francomarinozzi4@gmail.com>
2025-09-16 17:54:29 -0300

    camiones api viajes

--- src/main/java/com/acme/tarifas/gestion/clients/ViajesClient.java
+++ src/main/java/com/acme/tarifas/gestion/clients/ViajesClient.java
@@ -1,8 +1,11 @@
 package com.acme.tarifas.gestion.clients;
 
+
 import com.acme.tarifas.gestion.dto.TransportistaDTO;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
@@ -10,7 +13,7 @@ import java.util.List;
 
 @Service
 public class ViajesClient {
-
+    //Sirve para consumir todo lo de la API de viajes.
     private final WebClient webClient;
 
     public ViajesClient(@Value("${VIAJES_URL}") String urlViajes, WebClient.Builder builder) {
@@ -30,7 +33,6 @@ public class ViajesClient {
                 .collectList()
                 .block();
     }
-
     /*
     Para concatenar el campo 'telefono':
     String numeroCompleto = "+" + contacto.getTelefono().getCodigoPais()
@@ -40,5 +42,14 @@ public class ViajesClient {
      VIAJES tiene 'telefono' como una entidad aparte, osea que solo nos llega la referencia mediante un id de mongo
      */
 
+
+    public List<JsonNode> getCamiones(){ //Vehiculos
+        return webClient.get()
+                .uri("/vehiculo")
+                .retrieve()
+                .bodyToFlux(JsonNode.class)
+                .collectList()
+                .block();
+         }
 
 }
---
+++ src/main/java/com/acme/tarifas/gestion/controller/CamionController.java
@@ -0,0 +1,27 @@
+package com.acme.tarifas.gestion.controller;
+
+import com.acme.tarifas.gestion.clients.ViajesClient;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/camiones")
+public class CamionController {
+
+    private ViajesClient viajesClient;
+
+    public CamionController(ViajesClient viajesClient) {
+        this.viajesClient = viajesClient;
+    }
+
+    @GetMapping
+    public List<JsonNode> getCamiones() {
+        return viajesClient.getCamiones();
+    }
+
+
+}
